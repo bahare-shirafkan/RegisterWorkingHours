66c20da5dcb86bac5a8b86435c789291be1c60d9
bahare shirafkan <bahare.shirafkan13@gmail.com>
2022-12-14 04:55:52 +0330

    create hour migration

---
+++ database/migrations/2022_12_13_211231_create_hours_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Hour;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hours', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp(Hour::COL_DATE);
+            $table->string(Hour::COL_FROM_TIME);
+            $table->string(Hour::COL_TO_TIME);
+            $table->foreignIdFor(User::class)->constrained();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hours');
+    }
+};

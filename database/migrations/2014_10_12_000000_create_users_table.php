7d5ed88adc0720ef872c1c6e8361eb83f1ec1a36
bahare shirafkan <bahare.shirafkan13@gmail.com>
2022-12-14 04:55:35 +0330

    create users migration

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string(User::COL_FNAME);
+            $table->string(User::COL_LNAME);
+            $table->string(User::COL_EMAIL)->unique();
+            $table->string(User::COL_PASSWORD);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};

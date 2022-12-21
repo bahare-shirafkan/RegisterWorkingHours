7459fa6a26024a0e8af7d6d51262036435f55419
bahare shirafkan <bahare.shirafkan13@gmail.com>
2022-12-22 03:16:09 +0330

    add login user and hour routes for web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HourController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+
+Route::get('/', [AuthController::class, 'index']);
+Route::get('login', [AuthController::class, 'index']);
+Route::post('login', [AuthController::class, 'login'])->name('login-store');
+
+Route::group(['middleware' => 'web'], function () {
+    Route::post('/user', [UserController::class, 'create']);
+    Route::get('/user', [UserController::class, 'index']);
+    Route::post('/hour', [HourController::class, 'create'])->name('hour-store')->middleware('auth:web');
+    Route::get('hour', [HourController::class, 'index']);
 });

bfc6fa47f5ba93d9a85c1ced754d97222ea4b5e7
bahare shirafkan <bahare.shirafkan13@gmail.com>
2022-12-22 03:10:50 +0330

    add value filed  and modelFilter to hour model

--- app/Models/Hour.php
+++ app/Models/Hour.php
@@ -2,21 +2,23 @@
 
 namespace App\Models;
 
+use App\ModelFilters\HourFilter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use EloquentFilter\Filterable;
 
 class Hour extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Filterable;
 
     const COL_ID = 'id';
     const COL_USER_ID = 'user_id';
     const COL_DATE = 'date';
     const COL_FROM_TIME = 'from_time';
     const COL_TO_TIME = 'to_time';
+    const COL_VALUE = 'value';
     const COL_CREATED_BY = 'created_by';
 
     protected $fillable = [
@@ -25,8 +27,18 @@ class Hour extends Model
         self::COL_DATE,
         self::COL_FROM_TIME,
         self::COL_TO_TIME,
+        self::COL_VALUE
     ];
 
+    protected $casts = [
+        self::COL_DATE => 'date',
+        self::COL_VALUE => 'float'
+    ];
+
+    public function modelFilter()
+    {
+        return $this->provideFilter(HourFilter::class);
+    }
 
     public function user(): BelongsTo
     {

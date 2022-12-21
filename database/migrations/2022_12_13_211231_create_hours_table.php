c30df2e391ccb031bf4191f8fc4bd33a5f39e6ab
bahare shirafkan <bahare.shirafkan13@gmail.com>
2022-12-22 03:13:14 +0330

    add value field to hour migration

--- database/migrations/2022_12_13_211231_create_hours_table.php
+++ database/migrations/2022_12_13_211231_create_hours_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp(Hour::COL_DATE);
             $table->string(Hour::COL_FROM_TIME);
             $table->string(Hour::COL_TO_TIME);
+            $table->string(Hour::COL_VALUE)->nullable();
             $table->foreignIdFor(User::class)->constrained();
             $table->timestamps();
             $table->softDeletes();

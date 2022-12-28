5174dd73b7b4e0615efbac6c122bb82ff672d3cd
bahare shirafkan <bahare.shirafkan13@gmail.com>
2022-12-29 01:45:47 +0330

    add minutes and houre to date and diffrence start time and end time

--- app/Http/Services/HourService.php
+++ app/Http/Services/HourService.php
@@ -4,9 +4,9 @@ namespace App\Http\Services;
 
 use App\ModelFilters\HourFilter;
 use App\Models\Hour;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\DB;
 
 class HourService
@@ -31,12 +31,21 @@ class HourService
         try {
             $user_id = 1;
             foreach ($request['items'] as $item) {
+                $times_end = explode(':', $item['to_time']);
+                $hour_end=$times_end[0];
+                $min_end=$times_end[1];
+                $end_time=Carbon::parse($item['date'])->addHours($hour_end)->addMinutes($min_end);
+                $times_start = explode(':', $item['from_time']);
+                $hour_start=$times_start[0];
+                $min_start=$times_start[1];
+                $start_time=Carbon::parse($item['date'])->addHours($hour_start)->addMinutes($min_start);
+                $dif_time = Carbon::parse($end_time)->diffInMinutes(Carbon::parse($start_time));
                 if (!empty($item['date']))
                     Hour::create([
                         Hour::COL_DATE => $item['date'],
                         Hour::COL_FROM_TIME => $item['from_time'],
                         Hour::COL_TO_TIME => $item['to_time'],
-                        Hour::COL_VALUE => (intval($item['to_time']) - intval($item['from_time'])) * 70000,
+                        Hour::COL_DIFF_TIME => $dif_time,
                         Hour::COL_USER_ID => $user_id,
                     ]);
             }

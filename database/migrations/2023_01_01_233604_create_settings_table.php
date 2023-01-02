caeb8f41d0982355d33cf33fd53ac6c0aa13ec0b
bahare shirafkan <bahare.shirafkan13@gmail.com>
2023-01-02 03:32:14 +0330

    create setting migration

---
+++ database/migrations/2023_01_01_233604_create_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Setting;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string(Setting::COL_SALARY_HOUR);
+            $table->string(Setting::COL_UNIT);
+            $table->foreignIdFor(User::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};

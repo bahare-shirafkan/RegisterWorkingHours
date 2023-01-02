4b5157f051945e6c1003e0e8e39878edf4b6b02d
bahare shirafkan <bahare.shirafkan13@gmail.com>
2023-01-02 20:13:00 +0330

    createheader page

---
+++ resources/views/Layout/header.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+</head>
+
+<body>
+    <div class='dashboard-app'>
+        <div class='dashboard-content'>
+            <div class='container'>
+                <div class="row">
+                    <div class="col-6">
+                        <img class="profile" src="{{asset('/img/personal-defult.png')}}" alt="">
+                        <h4 class="mt-4">Bahare Shirafkan</h4>
+                    </div>
+                    <!-- <div class="col-7"> -->
+                    <!-- </div> -->
+                </div>

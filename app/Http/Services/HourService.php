c0282f84feedaa28ee61ce04e6b86ba59b5459e8
bahare shirafkan <bahare.shirafkan13@gmail.com>
2023-02-08 01:51:59 +0330

    change persian number to english and convert persian date to georgian

--- app/Http/Services/HourService.php
+++ app/Http/Services/HourService.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Services;
 
-use App\ModelFilters\HourFilter;
+use App\Jobs\SendTransactionToZoho;
 use App\Models\Hour;
 use App\Models\Setting;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Morilog\Jalali\Jalalian;
+use EasyJalali\JalaliDate;
 
 class HourService
 {
@@ -17,18 +19,17 @@ class HourService
         $user_id = Auth::user()->id ?? 1;
         $setting = Setting::where(Setting::COL_USER_ID, $user_id)->first();
         // $sum = 0;
-        $res= null;
-        $message=null;
+        $res = null;
+        $message = null;
         if (isset($setting) && !empty($setting)) {
-            $res= Hour::where(Hour::COL_USER_ID, $user_id)->get();
+            $res = Hour::where(Hour::COL_USER_ID, $user_id)->get();
             // foreach ($res as $item) {
             //     $sum += $item['diff_time'] * 70000;
             // }
         } else {
             // TODO:i do its after
-            $message="i do its after";
+            $message = "i do its after";
         }
-
         return view('list', compact('res', 'setting','message'));
     }
 
@@ -38,18 +39,30 @@ class HourService
         try {
             $user_id = 1;
             foreach ($request['items'] as $item) {
-                $times_end = explode(':', $item['to_time']);
-                $hour_end = $times_end[0];
-                $min_end = $times_end[1];
-                $end_time = Carbon::parse($item['date'])->addHours($hour_end)->addMinutes($min_end);
+                // Convert Persian numbers to English
+                $date_english = to_english_numbers($item['date']);
+                // Convert jalali date to georgian
+                $jDate = Jalalian::fromFormat('Y/m/d', $date_english)->toCarbon()->format('Y-m-d');
+
+                // create time format(start time)
                 $times_start = explode(':', $item['from_time']);
                 $hour_start = $times_start[0];
                 $min_start = $times_start[1];
-                $start_time = Carbon::parse($item['date'])->addHours($hour_start)->addMinutes($min_start);
+                $start_time = Carbon::parse($jDate)->addHours($hour_start)->addMinutes($min_start);
+
+                // create time format(end time)
+                $times_end = explode(':', $item['to_time']);
+                $hour_end = $times_end[0];
+                $min_end = $times_end[1];
+                $end_time = Carbon::parse($jDate)->addHours($hour_end)->addMinutes($min_end);
+
+                // calculate diffrent two time
                 $dif_time = Carbon::parse($end_time)->diffInMinutes(Carbon::parse($start_time));
+
+                // insert date in table
                 if (!empty($item['date']))
                     Hour::create([
-                        Hour::COL_DATE => $item['date'],
+                        Hour::COL_DATE => $jDate,
                         Hour::COL_FROM_TIME => $item['from_time'],
                         Hour::COL_TO_TIME => $item['to_time'],
                         Hour::COL_DIFF_TIME => $dif_time,
